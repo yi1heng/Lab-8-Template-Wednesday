072d4ac481f18d0cf16fdb9116797ef5be6ae2c6
yi1heng <1hengfu@gmail.com>
2021-03-22 12:50:59 -0600

    add new test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,11 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testHasEdmonton() {
+        CityList cityList = mockCityList();
+
+        assertTrue(cityList.hasEdmonton());
+    }
 }
